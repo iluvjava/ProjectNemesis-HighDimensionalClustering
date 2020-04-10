9855b3be764de09eba0a47c8334ce67a06317f89
Alto Legato <victor1301166040@gmail.com>
2020-04-10 00:20:20 -0700

    New shit

---
+++ Project_Nemesis__HighDimensionalClustering/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Project_Nemesis__HighDimensionalClustering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
